07395f3e445c040bdc8752b9b094c170fc1b9f3f
Ho Minh Duc <duchm@mlo.vn>
2022-10-07 16:58:18 +0700

    add button state

--- src/components/shareButton/index.js
+++ src/components/shareButton/index.js
@@ -1,16 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { StyledButton } from "./styles";
 
 export const ShareButton = () => {
   const notify = (text) => toast(text);
+  const [shareSupport, setShareSupport] = useState(null)
 
   function handlerError(error) {
     notify(error);
+    setShareSupport(false)
   }
 
   function handlerSupportFunc() {
     toast({content: 'Hỗ trợ chức năng share'})
+    setShareSupport(true)
   }
 
   function share(text) {
@@ -24,7 +27,7 @@ export const ShareButton = () => {
 
   return (
     <StyledButton onClick={() => share("I want to share this text")}>
-      Share URL
+      Share URL <div><b>{shareSupport === true ? 'Support':'' } {shareSupport === false ? 'Unsupport':'' }</b></div>
     </StyledButton>
   );
 };

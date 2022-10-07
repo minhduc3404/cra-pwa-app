4b16666a84157544f122a308b78017ea1b8713b2
Ho Minh Duc <duchm@mlo.vn>
2022-10-07 11:28:16 +0700

    as

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // useEffect(() => {
+  //   console.log('allow share')
+  //   let url = document.location.href;
+  //   const canonicalElement = document.querySelector('link[rel=canonical]');
+  //   if (canonicalElement !== null) {
+  //       url = canonicalElement.href;
+  //   }
+    
+  //   navigator.share({
+  //     title: 'Codica',
+  //     text: 'Codica',
+  //     url
+  //   })
+  //     .then(() => console.log('Successful share'))
+  //     .catch((error) => console.log('Error sharing', error));
+  // }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,7 +34,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React ass
         </a>
       </header>
     </div>
